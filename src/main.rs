c20e82661dc0f683adddf8db75e91a6074f6212e
9.5 <fourandahalf2@gmail.com>
2026-04-26 18:54:29 +0200

    add saving data to a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{env::consts::ARCH, fs::File, io::Write};
+
 use clap::Parser;
 mod sieves;
 
@@ -7,15 +9,34 @@ struct Args {
     /// Limit of sieve
     #[arg(short, long)]
     limit: usize,
+
+    /// Place the output into file
+    #[arg(short,long, default_value_t = String::from("out.txt"))]
+    output: String,
+
+    /// Display primes
+    #[clap(short, long)]
+    display: bool,
 }
 
-fn main() {
+fn main()  -> Result<(), Box<dyn std::error::Error>>{
     let args = Args::parse();
     let mut sieve = sieves::SieveOfEratosthenes::new(args.limit);
-   
-   let primes = sieve.get_primes();
 
-   for prime in primes{
-    println!("{}",prime)
-   }
+    let primes = sieve.get_primes();
+    if args.display {
+        for prime in primes {
+            println!("{}", prime);
+        }
+    }
+    else {
+        let mut file = File::create( args.output)?;
+
+        for prime in primes{
+            let _ = writeln!(file,"{}",prime);
+        }
+    }
+
+
+    Ok(())
 }
